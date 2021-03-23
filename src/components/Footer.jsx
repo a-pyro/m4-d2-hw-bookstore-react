41f3a74b6e1b337cf89e2bb07f5d762fbda00fd4
ardigermenji <ardi.germenji@gmail.com>
2021-03-23 15:41:29 +0100

    adds navbar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Row, Col } from 'react-bootstrap';
+
+export default function Footer() {
+  return (
+    <Row className='fixed-bottom bg-dark text-white justify-content-center'>
+      <Col>shy</Col>
+      <Col>asd</Col>
+      <Col>asd</Col>
+      <Col>asd</Col>
+    </Row>
+  );
+}
